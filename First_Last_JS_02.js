179a590ac73011b2db9fb0927c1f806db329f9ab
Twig30 <immanuel.delgado@stu.vertexacademies.org>
2024-10-16 14:07:41 -0400

    Add files via upload

---
+++ First_Last_JS_02.js
@@ -0,0 +1,14 @@
+// Example:             // Expected Output
+console.log(4 + 2.5)   // 6.5
+
+// Write 5 more expressions below:
+console.log(6+ 0.5); // 6.5
+console.log(13-6.5); // 6.5
+console.log(13*0.5); // 6.5
+console.log(13/2); // 6.5
+console.log((2*3)+0.5); // 6.5
+console.log(!true);
+console.log(!!false);
+console.log(true||true);
+console.log(false||true);
+
