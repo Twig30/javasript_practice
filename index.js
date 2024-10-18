91b4c3fd781baf3bb9ed793de98a6bef7f17a66f
Twig30 <immanuel.delgado@stu.vertexacademies.org>
2024-10-18 14:17:17 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+// 1. Declare a variable called `firstName` and assign it your first name as a string:
+let firstname= "Immanuel";
+
+// 2. Declare a variable called `lastName` and assign it your last name as a string:
+let lastname= "Delgado";
+
+// 3. Declare a variable called `age`:
+let age= 16;
+
+// 4. Print out the `firstName`, `lastName`, and `age` variables. What do you
+//    expect them to be when they get printed?
+console.log(firstname,lastname);
+console.log(age)
+
+// 5. Assign the `age` variable to a number:
+
+
+// 6. Print out the `firstName`, `lastName`, and `age` variables. What do you
+//    expect them to be when they get printed?
